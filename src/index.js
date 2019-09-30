7e1e94f79aab687b9f00d4556975f5df40ddb95a
randy <randydxp@gmail.com>
2019-09-30 11:08:55 -0500

    added mapbox dependency, styling, and initial marker

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log("Hello world");
+const mapbox = require("mapbox-gl");
+
+mapbox.accessToken = "pk.eyJ1Ijoic2hpcmFuaWFvcmkiLCJhIjoiY2sxNmt1bzYxMTZ3YTNibnhpa3U1ZTVnbiJ9.eITx5ki4ZOghrvkSAFWn2Q";
+
+const map = new mapbox.Map({
+  container: "map",
+  center: [-87.641, 41.895],
+  zoom: 12,
+  style: "mapbox://styles/mapbox/streets-v10"
+});
+
+const myDiv = document.createElement('div');
+myDiv.style.height = '10vh';
+myDiv.style.width = '10vw';
+myDiv.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";
+myDiv.classList.add('green-marker');
+document.getElementById('map').appendChild(myDiv);
+new mapbox.Marker(myDiv)
+  .setLngLat([-87.641, 41.895])
+  .addTo(map);
